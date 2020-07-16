33c3565de3da9468901de8395091f7d7b8e63b23
JinWen <onlystp417@gmail.com>
2020-07-16 15:56:40 +0800

    feat: add task

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 // 取得 DOM 節點
 const main = document.querySelector('main');
 const addTaskButton = document.querySelector('.add-form');
-const taskList = document.querySelector('.tasks-list');
 const task = document.querySelector('.task');
+const taskSaveButton = document.querySelector('.task-btn-save');
 
 // 綁定事件
 addTaskButton.addEventListener('click', showTaskCard);
 main.addEventListener('click', hideTaskCard);
 
+// 代辦事項 data
 const taskListArray = [
   {
     name: '刷牙',
-    date: '2020-7-16',
+    date: '2020-07-16',
     time: '21:30',
     comment: '再不刷就要蛀牙了',
     file: '123.png',
@@ -21,7 +22,7 @@ const taskListArray = [
   },
   {
     name: '吃飯',
-    date: '2020-7-16',
+    date: '2020-07-16',
     time: '18:30',
     comment: '',
     file: '',
@@ -31,7 +32,7 @@ const taskListArray = [
   },
   {
     name: '整理讀書會筆記',
-    date: '2020-7-20',
+    date: '2020-07-20',
     time: '22:30',
     comment: '整理忍者第五章',
     file: '',
@@ -47,8 +48,6 @@ function showTaskCard(e) {
   e.stopPropagation();
   // 顯示編輯任務區域
   task.classList.remove('d-none');
-  // 取得 data 們的節點
-  const taskSaveButton = document.querySelector('.task-btn-save');
 
   taskSaveButton.addEventListener('click', taskSave);
 }
@@ -56,6 +55,7 @@ function showTaskCard(e) {
 renderTaskList();
 
 function renderTaskList() {
+  const taskList = document.querySelector('.tasks-list');
   let taskHTML = ''
   taskListArray.forEach((item, index) => {
     taskHTML += `
@@ -63,7 +63,7 @@ function renderTaskList() {
         <header class="task-title">
           <h2>
             <span class="checkbox">
-              <input class="check-itself" id="check-${ index + 1 }" type="checkbox" >
+              <input class="check-itself" id="check-${ index + 1 }" type="checkbox" ${ item.isComplete ? 'checked' : '' }  >
               <label class="check-custom" for="check-${ index + 1 }"><i class="fas fa-check"></i></label>
             </span>
             <input class="task-name ${ item.isComplete ? 'cross-off' : '' }" type="text" placeholder="Type Something Here..." value="${ item.name }">
@@ -71,7 +71,7 @@ function renderTaskList() {
           <div class="task-mark">
             <span class="task-mark-star ${ item.star ? 'd-none' : '' }"><i class="far fa-star"></i></span>
             <span class="task-mark-star task-mark-star-shiny ${ item.star ? '' : 'd-none' }"><i class="fas fa-star"></i></span>
-            <span class="task-mark-pen current-pen"><i class="fas fa-pen"></i></span>
+            <span class="task-mark-pen"><i class="fas fa-pen"></i></span>
           </div>
           <div class="task-tag">
             <span class="tag-item tag-time ${ item.date ? '' : 'd-none' }">
@@ -90,15 +90,16 @@ function renderTaskList() {
             <section class="task-form-item task-form-deadline">
               <h3><i class="far fa-calendar-alt"></i>Deadline</h3>
               <div action="">
-                <input type="date" value="2020-01-01">
-                <input type="time" value="22:50">
+                <input type="date" value="${ item.date }">
+                <input type="time" value="${ item.time }">
               </div>
             </section>
             <section class="task-form-item task-form-file">
               <h3><i class="far fa-file"></i>File</h3>
+              <input type="file" data-keyname="file" value="${ item.file }" name="" id="">
               <div class="file-add">
                 <div class="file-caption">
-                  <h4>File Name</h4>
+                  <h4>${ item.file }</h4>
                   <time>upload yesterday</time>
                 </div>
                 <button class="file-add-button" type="menu"><i class="fas fa-plus"></i></button>
@@ -106,7 +107,7 @@ function renderTaskList() {
             </section>
             <section class="task-form-item task-form-comment">
               <h3><i class="far fa-comment-dots"></i>Comment</h3>
-              <textarea name="" id="" cols="30" rows="10" placeholder="Type your meno here..."></textarea>
+              <textarea name="" id="" cols="30" rows="10" placeholder="Type your meno here... value="${ item.comment }"></textarea>
             </section>
           </div>
           <div class="task-btn">
@@ -121,15 +122,15 @@ function renderTaskList() {
 }
 
 function taskSave() {
-  const inputs = task.querySelectorAll('.task-data');
+  const inputs = task.querySelectorAll('input');
   const newTask = {};
   inputs.forEach(input => {
-    newTask[input.dataset.keyname] = input.value;
+    newTask[input.dataset.keyname] = (input.value !== 'on') 
+      ? input.value : input.checked
+      ? true : false;
   });
-  taskListArray.push(newTask);
-
-  console.log(taskListArray);
-
+  taskListArray.unshift(newTask);
+  renderTaskList();
 }
 
 function hideTaskCard(e) {
