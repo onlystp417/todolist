494929febd6dc69cedbdf7f1163c3621310d8581
JinWen <onlystp417@gmail.com>
2020-08-20 13:51:33 +0800

    refactor: build Dtesk element tear into several function

--- js/main.js
+++ js/main.js
@@ -7,15 +7,13 @@ const taskCancelButton = document.querySelector('.task-btn-cancel');
 const tags = document.querySelectorAll('.label-link');
 const taskCounter = document.querySelector('.task-counter > span');
 
-taskAddingForm.addEventListener('click', e => {
-  e.stopPropagation();
-});
+taskAddingForm.addEventListener('click', e => e.stopPropagation());
 
 // 綁定事件
 openTaskFormButton.addEventListener('click', showTaskCard);
 taskCancelButton.addEventListener('click', hideTaskCard);
 baseLayer.addEventListener('click', hideTaskCard);
-tags.forEach(tag => tag.addEventListener('click', switchTag))
+tags.forEach(tag => tag.addEventListener('click', switchTag));
 
 // 初始化原始資料
 const taskListArray = JSON.parse(localStorage.getItem('taskList')) || [];
@@ -29,74 +27,119 @@ renderTaskList();
 // 將所有任務渲染在畫面上
 function renderTaskList() {
   const taskList = document.querySelector('.tasks-list');
-  taskList.addEventListener('click', e => {
-    e.stopPropagation();
-  })
-  let taskHTML = taskListShow.map((item, index) => {
-    return `
-      <form class="task" id="task-item-${index + 1}">
-        <header class="task-title ${item.isStar ? 'is-star' : ''}">
-          <h2>
-            <span class="checkbox">
-              <input class="check-is-complete task-data" id="check-${ index + 1 }" type="checkbox" ${ item.isComplete ? 'checked' : '' } data-keyname="isComplete" >
-              <label class="check-custom" for="check-${ index + 1 }"><i class="fas fa-check"></i></label>
-            </span>
-            <input onkeypress="if (event.keyCode == 13) {return false;}" class="task-name task-data ${ item.isComplete ? 'cross-off' : '' }" type="text" placeholder="Type Something Here..." value="${ item.name }" data-keyname="name">
-          </h2>
-          <div class="task-mark">
-            <input class="task-mark-star task-data" id="isStar-${ index + 1 }" type="checkbox" data-keyname="isStar" ${ item.isStar ? 'checked' : '' } data-keyname="isStar">
-            <label class="task-mark-star-custom" for="isStar-${ index + 1 }"></label>
-            <input class="task-mark-pen" id="isEdit-${ index + 1 }" type="checkbox" data-keyname="isEdit">
-            <label class="task-mark-pen-custom" for="isEdit-${ index + 1 }" id="edit-pen-${index + 1}"></label>
-            <button type="submit" class="task-mark-delete" id="delete-${ index + 1 }"><i class="far fa-trash-alt"></i></button>
-          </div>
-          <div class="task-tag">
-            <span class="tag-item tag-time ${ item.date ? '' : 'd-none' }">
-              <i class="far fa-calendar-alt"></i><time>${item.date}</time>
-            </span>
-            <span class="tag-item tag-file ${ item.file ? '' : 'd-none' }">
-              <i class="far fa-file"></i>
-            </span>
-            <span class="tag-item tag-comment ${ item.comment ? '' : 'd-none' }">
-              <i class="far fa-comment-dots"></i>
-            </span>
-          </div>
-        </header>
-        <section class="task-form ${item.isEdit ? '' : 'd-none'}">
-          <div class="task-form-edit">
-            <section class="task-form-item task-form-deadline">
-              <h3><i class="far fa-calendar-alt"></i>Deadline</h3>
-              <div action="">
-                <input class="task-data" type="date" value="${ item.date }" data-keyname="date">
-                <input class="task-data" type="time" value="${ item.time }" data-keyname="time">
-              </div>
-            </section>
-            <section class="task-form-item task-form-file">
-              <h3><i class="far fa-file"></i>File</h3>
-              <div class="file-caption">
-                <h4>${ item.file }</h4>
-                <time>${ item.fileTime }</time>
-              </div>
-              <label class="file-add-button" for="add-file-${ index + 1 }"><i class="fas fa-plus"></i></label>
-              <input class="task-data file-input" type="file" data-keyname="file" id="add-file-${ index + 1 }">              <div class="file-add">
-            </section>
-            <section class="task-form-item task-form-comment">
-              <h3><i class="far fa-comment-dots"></i>Comment</h3>
-              <textarea class="task-data" name="" id="" cols="3" rows="3" placeholder="Type your meno here..." data-keyname="comment">${ item.comment }</textarea>
-            </section>
-          </div>
-          <div class="task-btn">
-            <button class="task-btn-basic task-btn-cancel" id="save-button-${index + 1}"><i class="fas fa-times"></i>Cancel</button>
-            <button class="task-btn-basic task-btn-save" id="cancel-button-${index + 1}"><i class="fas fa-plus"></i>Save</button>
-          </div>
-        </section>
-      </form>
-    `;
-  })
-  taskList.innerHTML = taskHTML.join('');
+  taskList.addEventListener('click', e => e.stopPropagation());
+
+  // const taskElement = ;
+
+  let taskListHTML = taskListShow.map((item, index) => buildTaskForm(item, index));
+
+  taskList.innerHTML = taskListHTML.join('');
 
   addEvent4TaskStatus();
 }
+
+// 建立 elements 以下拆 module
+function buildTaskForm(item, index) {
+  return `
+  <form class="task" id="task-item-${index + 1}">
+    ${ buildTaskHeader(item, index) }
+    ${ buildTaskBody(item, index) }
+  </form>
+  `
+}
+
+function buildTaskHeader(item, index) {
+  return `
+  <header class="task-title ${item.isStar ? 'is-star' : ''}">
+    ${ buildTaskTitle(item, index) }
+    ${ buildTaskMark(item, index) }
+    ${ buildTaskTag(item, index) }
+  </header>
+  `
+}
+
+function buildTaskTitle(item, index) {
+  return `
+  <h2>
+    <span class="checkbox">
+      <input class="check-is-complete task-data" id="check-${ index + 1 }" type="checkbox" ${ item.isComplete ? 'checked' : '' } data-keyname="isComplete" >
+      <label class="check-custom" for="check-${ index + 1 }"><i class="fas fa-check"></i></label>
+    </span>
+    <input onkeypress="if (event.keyCode == 13) {return false;}" class="task-name task-data ${ item.isComplete ? 'cross-off' : '' }" type="text" placeholder="Type Something Here..." value="${ item.name }" data-keyname="name">
+  </h2>
+  `
+}
+
+function buildTaskMark(item, index) {
+  return `
+  <div class="task-mark">
+    <input class="task-mark-star task-data" id="isStar-${ index + 1 }" type="checkbox" data-keyname="isStar" ${ item.isStar ? 'checked' : '' } data-keyname="isStar">
+    <label class="task-mark-star-custom" for="isStar-${ index + 1 }"></label>
+    <input class="task-mark-pen" id="isEdit-${ index + 1 }" type="checkbox" data-keyname="isEdit">
+    <label class="task-mark-pen-custom" for="isEdit-${ index + 1 }" id="edit-pen-${index + 1}"></label>
+    <button type="submit" class="task-mark-delete" id="delete-${ index + 1 }"><i class="far fa-trash-alt"></i></button>
+  </div>
+
+  `
+}
+
+function buildTaskTag(item) {
+  return `
+  <div class="task-tag">
+    <span class="tag-item tag-time ${ item.date ? '' : 'd-none' }">
+      <i class="far fa-calendar-alt"></i><time>${item.date}</time>
+    </span>
+    <span class="tag-item tag-file ${ item.file ? '' : 'd-none' }">
+      <i class="far fa-file"></i>
+    </span>
+    <span class="tag-item tag-comment ${ item.comment ? '' : 'd-none' }">
+      <i class="far fa-comment-dots"></i>
+    </span>
+  </div>
+
+  `
+}
+
+function buildTaskBody(item, index) {
+  return `
+  <section class="task-form ${item.isEdit ? '' : 'd-none'}">
+    <div class="task-form-edit">
+      <section class="task-form-item task-form-deadline">
+        <h3><i class="far fa-calendar-alt"></i>Deadline</h3>
+        <div action="">
+          <input class="task-data" type="date" value="${ item.date }" data-keyname="date">
+          <input class="task-data" type="time" value="${ item.time }" data-keyname="time">
+        </div>
+      </section>
+      <section class="task-form-item task-form-file">
+        <h3><i class="far fa-file"></i>File</h3>
+        <div class="file-caption">
+          <h4>${ item.file }</h4>
+          <time>${ item.fileTime }</time>
+        </div>
+        <label class="file-add-button" for="add-file-${ index + 1 }"><i class="fas fa-plus"></i></label>
+        <input class="task-data file-input" type="file" data-keyname="file" id="add-file-${ index + 1 }">              <div class="file-add">
+      </section>
+      <section class="task-form-item task-form-comment">
+        <h3><i class="far fa-comment-dots"></i>Comment</h3>
+        <textarea class="task-data" name="" id="" cols="3" rows="3" placeholder="Type your meno here..." data-keyname="comment">${ item.comment }</textarea>
+      </section>
+    </div>
+    ${ buildTaskButton(item, index) }
+  </section>
+  `
+}
+
+function buildTaskButton(index) {
+  return `
+  <div class="task-btn">
+    <button class="task-btn-basic task-btn-cancel" id="save-button-${index + 1}"><i class="fas fa-times"></i>Cancel</button>
+    <button class="task-btn-basic task-btn-save" id="cancel-button-${index + 1}"><i class="fas fa-plus"></i>Save</button>
+  </div>
+  `
+}
+
+// 以上拆 module
 
 // 將資料存進 localStorage
 function storeData() {
@@ -115,7 +158,13 @@ function switchTag(e) {
 
 // 過濾出當前頁籤的資料
 function filterTaskList(type) {
-  let items = [];
+  // let items = [];
+  // const obj = {
+  //   progress: items = taskListArray.filter(item => item.isComplete === false),
+
+  // };
+  // obj[type];
+
   switch(type) {
     case 'progress':
       items = taskListArray.filter(item => item.isComplete === false);
