7d93677a28fef2f9ceb4da60036597f3d6fdfe8f
JinWen <onlystp417@gmail.com>
2020-08-21 15:18:00 +0800

    refactor: build buildElement module

--- js/main.js
+++ js/main.js
@@ -31,64 +31,6 @@ function renderTaskList() {
   showLeftTasks();
 }
 
-function buildTaskTag(item) {
-  return `
-  <div class="task-tag">
-    <span class="tag-item tag-time ${ item.date ? '' : 'd-none' }">
-      <i class="far fa-calendar-alt"></i><time>${item.date}</time>
-    </span>
-    <span class="tag-item tag-file ${ item.file ? '' : 'd-none' }">
-      <i class="far fa-file"></i>
-    </span>
-    <span class="tag-item tag-comment ${ item.comment ? '' : 'd-none' }">
-      <i class="far fa-comment-dots"></i>
-    </span>
-  </div>
-
-  `
-}
-
-function buildTaskBody(item, index) {
-  return `
-  <section class="task-form ${item.isEdit ? '' : 'd-none'}">
-    <div class="task-form-edit">
-      <section class="task-form-item task-form-deadline">
-        <h3><i class="far fa-calendar-alt"></i>Deadline</h3>
-        <div action="">
-          <input class="task-data" type="date" value="${ item.date }" data-keyname="date">
-          <input class="task-data" type="time" value="${ item.time }" data-keyname="time">
-        </div>
-      </section>
-      <section class="task-form-item task-form-file">
-        <h3><i class="far fa-file"></i>File</h3>
-        <div class="file-caption">
-          <h4>${ item.file }</h4>
-          <time>${ item.fileTime }</time>
-        </div>
-        <label class="file-add-button" for="add-file-${ index + 1 }"><i class="fas fa-plus"></i></label>
-        <input class="task-data file-input" type="file" data-keyname="file" id="add-file-${ index + 1 }">              <div class="file-add">
-      </section>
-      <section class="task-form-item task-form-comment">
-        <h3><i class="far fa-comment-dots"></i>Comment</h3>
-        <textarea class="task-data" name="" id="" cols="3" rows="3" placeholder="Type your meno here..." data-keyname="comment">${ item.comment }</textarea>
-      </section>
-    </div>
-    ${ buildTaskButton(item, index) }
-  </section>
-  `
-}
-
-function buildTaskButton(index) {
-  return `
-  <div class="task-btn">
-    <button class="task-btn-basic task-btn-cancel" id="save-button-${index + 1}"><i class="fas fa-times"></i>Cancel</button>
-    <button class="task-btn-basic task-btn-save" id="cancel-button-${index + 1}"><i class="fas fa-plus"></i>Save</button>
-  </div>
-  `
-}
-
-// 以上拆 module
-
 // 將資料存進 localStorage
 function storeData() {
   localStorage.setItem('taskList', JSON.stringify(taskListArray));
@@ -106,13 +48,12 @@ function switchTag(e) {
 
 // 過濾出當前頁籤的資料
 function filterTaskList(type) {
-  // let items = [];
+  let items = [];
   // const obj = {
   //   progress: items = taskListArray.filter(item => item.isComplete === false),
 
   // };
   // obj[type];
-
   switch(type) {
     case 'progress':
       items = taskListArray.filter(item => item.isComplete === false);
