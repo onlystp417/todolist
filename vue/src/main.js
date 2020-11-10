c6726521147fe4f701114f4f09a08ae1042efcce
JinWen <onlystp417@gmail.com>
2020-11-10 10:34:15 +0800

    [Build] Build FontAwsome as module

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import './index.css'
+
+import '@fortawesome/fontawesome-free/js/all.js';
 import './assets/scss/main.scss'
 
 createApp(App).mount('#app')
