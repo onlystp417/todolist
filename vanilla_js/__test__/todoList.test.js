a13895cc914b4271acf16e82abbfdb37c6c602e3
JinWen <onlystp417@gmail.com>
2020-11-25 17:26:09 +0800

    [Build] Test dependencies: Jest, testing-library-dom, babel, babel-jest

---
+++ vanilla_js/__test__/todoList.test.js
@@ -0,0 +1,5 @@
+// 要從 @testing-library/dom 中 import 一些必要的 method
+// 查找 element 的 getByText 和 getByTestId 還有執行事件觸發的 fireEvent
+const {
+  getByText, getByTestId, fireEvent
+} = require('@testing-library/dom');
